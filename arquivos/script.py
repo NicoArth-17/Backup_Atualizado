1e2d79cdb9bf948283fc068d943121c812ff7401
Nicolas Arthur <nicolarthur17@hotmail.com>
2023-06-18 19:32:08 -0300

    backup finalizado

---
+++ arquivos/script.py
@@ -0,0 +1,53 @@
+from pathlib import Path # interagir com arquivos do pc
+import shutil as sh # copiar arquivos e diretórios por completo
+
+# ATUALIZANDO PASTA PYTHON
+
+# Selecionando pasta Python do pc e do pen drive
+file_py_pc = Path(r'C:\Users\mobishopgamer\Documents\Estudo\Hashtag\Python')
+file_py_pd = Path(r'D:\Python')
+
+# lista de caminhos dos arquivos da pasta Python no pc
+list01_pc = [file for file in file_py_pc.iterdir()]
+# lista com caminho e nome dos arquivos da pasta Python no pen drive
+list01_pd = [file for file in file_py_pd.iterdir()]
+list01_pd_name = [file.name for file in file_py_pd.iterdir()]
+
+list02_pd_name = []
+
+list_teste_pc = []
+
+for archive_pd in list01_pd:
+    list02_pd_name_add = [list02_pd_name.append(file.name) for file in archive_pd.iterdir() if file.is_dir() or file.is_file()]
+
+# Percorrendo arquivos no pc
+for archive in list01_pc:
+    # se o arquivo do pc não existir no pen drive:
+    if not archive.name in list01_pd_name:
+        # criar cópia do diretório inteiro para o pen drive
+        sh.copytree(archive, rf'D:\Python\{archive.name}')
+
+    list02_pc = [file for file in archive.iterdir() if file.is_dir() or file.is_file()]
+
+    # list_teste_pc_add = [list_teste_pc.append(file.name) for file in archive.iterdir() if file.is_dir() or file.is_file()]
+
+    arquivos_backup = [file_noexist for file_noexist in list02_pc if not file_noexist.name in list02_pd_name]
+
+    for item in list02_pc:
+        if item in arquivos_backup:
+            if item.is_file():
+                sh.copy(item, rf'D:\Python\{archive.name}\{item.name}')
+            elif item.is_dir():
+                sh.copytree(item, rf'D:\Python\{archive.name}\{item.name}')
+        elif item.name == 'ArquivosAulas':
+            qntd_arquivos_pc = [file for file in item.iterdir() if file.is_dir() or file.is_file()]
+            qntd_arquivos_pd = [file.name for file in Path(r'D:\Python\Aulas\ArquivosAulas').iterdir() if file.is_dir() or file.is_file()]
+            if not len(qntd_arquivos_pc) == len(qntd_arquivos_pd):
+                for new_item in qntd_arquivos_pc:
+                    if not new_item.name in qntd_arquivos_pd:
+                        if new_item.is_file():
+                            sh.copy(new_item, rf'D:\Python\{archive.name}\{item.name}\{new_item.name}')
+                        elif new_item.is_dir():
+                            sh.copytree(new_item, rf'D:\Python\{archive.name}\{item.name}\{new_item.name}')
+
+print('backup realizado com sucesso!')
